a3178d592641854e655d7737114e738ce49df56a
13_chengjinpeng <cjp1036934016@163.com>
2019-07-27 14:48:29 +0800

    第一次提交项目-2019年7月27日14:48:27

---
+++ src/main/java/com/miaoshaproject/App.java
@@ -0,0 +1,16 @@
+package com.miaoshaproject;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableAutoConfiguration
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
